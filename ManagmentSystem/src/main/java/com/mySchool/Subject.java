54814913ec91522dcce2def84ecfd51b5fd12170
bb <beatriz.bravo-chavez@ten10.com>
2023-03-14 18:39:14 +0000

    created the meto getPass to check if the student has pass or fail a particular subject

--- ManagmentSystem/src/main/java/com/mySchool/Subject.java
+++ ManagmentSystem/src/main/java/com/mySchool/Subject.java
@@ -5,11 +5,17 @@ public class Subject {
     private String name;
     private double score;
 
+        //Constructor
+        // The atributes declare a nivel of the Class are going be modificate
+        // this variables, we declare are going be equal to the value we introduce in this level
+    // the method is publico y podemos acceder
     public Subject(String name, double score) {
         this.name = name;
         this.score = score;
     }
 
+    //get and setter methods
+    // they allow to access and modificate the values
     public String getName() {
         return name;
     }
@@ -26,5 +32,12 @@ public class Subject {
         this.score = score;
     }
 
+    // methods return if the student has pass or fail
+    public String getPass(){
+        if (score == 5) {
+            return "Pass";
+
+        }else return "Fail";
+    }
 
 }
